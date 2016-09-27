50ebd28842042d4508ac3cc3669db7030c97ad99
nixx78 <nixx78@gmail.com>
2016-09-27 14:22:18 +0300

    Unused import removed

--- gleif-poc/src/main/java/lv/nixx/poc/gleif/AppConfig.java
+++ gleif-poc/src/main/java/lv/nixx/poc/gleif/AppConfig.java
@@ -4,10 +4,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.apache.derby.jdbc.ClientDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
