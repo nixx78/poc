805ecb390005697ebb8ad0cbde01ec6f09921cc9
nixx78 <nixx78@gmail.com>
2016-10-07 11:37:36 +0300

    Initial project commit

---
+++ java8-poc/src/test/java/lv/nixx/poc/java8/txn/collection/TxnHolderTest.java
@@ -0,0 +1,47 @@
+package lv.nixx.poc.java8.txn.collection;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class TxnHolderTest {
+	
+	@Test
+	public void onlyLatesTxnShouldBeStored() throws ParseException {
+		
+		TxnHolder th = new TxnHolder();
+		th.add(new Transaction("id1", BigDecimal.valueOf(10.10), "ACC1.CHANGED", "USD", getDate("01.09.2016")));
+		th.add(new Transaction("id2", BigDecimal.valueOf(20.12), "ACC2", "USD", getDate("01.08.2016")));
+		th.add(new Transaction("id3", BigDecimal.valueOf(20.12), "ACC3", "USD", getDate("03.09.2016")));
+		th.add(new Transaction("id6", BigDecimal.valueOf(20.12), "ACC3.NEW", "USD", getDate("03.09.2016")));
+		th.add(new Transaction("id3", BigDecimal.valueOf(20.12), "ACC3.CHANGED", "USD", getDate("03.09.2016")));
+		th.add(new Transaction("id3", BigDecimal.valueOf(20.12), "ACC3.CHANGED1", "USD", getDate("03.08.2016")));
+
+		System.out.println("--- Unique values ---");
+		final Collection<Transaction> values = th.getValues();
+		assertEquals(4, values.size());
+		
+		for (Transaction t : values) {
+			System.out.println(t);
+		}
+		
+		System.out.println("--- Duplicated values ---");
+		final Collection<Transaction> duplicatedValues = th.getDuplicatedValues();
+		assertEquals(2, duplicatedValues.size());
+		for (Transaction t : duplicatedValues) {
+			System.out.println(t);
+		}
+		
+	}
+	
+	private Date getDate(String date) throws ParseException {
+		return new SimpleDateFormat("dd.MM.yyyy").parse(date);
+	}
+
+}
---
+++ java8-poc/src/test/java/lv/nixx/poc/java8/txn/collection/TxnLambdaModification.java
@@ -0,0 +1,315 @@
+package lv.nixx.poc.java8.txn.collection;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+public class TxnLambdaModification {
+
+	@Test
+	public void functionCall() {
+
+		List<Transaction> txns = Arrays.asList(
+				new Transaction("id1", BigDecimal.valueOf(10.10), "ACC1", "USD"),
+				new Transaction("id2", BigDecimal.valueOf(20.12), "ACC2", "USD"),
+				new Transaction("id3", BigDecimal.valueOf(30.13), "ACC2", "EUR"),
+				new Transaction("id4", BigDecimal.valueOf(40.14), "ACC3", "EUR")
+				);
+		
+		TxnProcessor myProcessor = new TxnProcessor();
+		// сссылка на метод, у конкретного экземпляра класса
+		txns.stream().forEach(myProcessor::simpleMethod);
+		// вызов статического метода с параметрами
+		txns.stream().forEach(t -> TxnProcessor.staticMethod(t.getAmount(), t.getCurrency()));
+
+		// Moжно вызывать и вот так, поскольу, MyProcessor implements Consumer
+		MyProcessor mp = new MyProcessor();
+		txns.forEach(mp);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void transformCollectionToMapDuplicateKey() {
+		
+		Collection<Transaction> txnSet = new HashSet<>();
+		txnSet.add(new Transaction("id1", BigDecimal.valueOf(10.10), "ACC1", "USD"));
+		txnSet.add(new Transaction("id1", BigDecimal.valueOf(99.10), "ACC1", "USD"));
+		txnSet.add(new Transaction("id2", BigDecimal.valueOf(20.12), "ACC2", "USD"));
+		txnSet.add(new Transaction("id3", BigDecimal.valueOf(30.13), "ACC2", "EUR"));
+		txnSet.add(new Transaction("id4", BigDecimal.valueOf(40.14), "ACC3", "EUR"));
+		
+		txnSet.stream().collect(Collectors.toMap(t-> t.getId(), Function.identity()));
+	}
+	
+	
+	@Test
+	public void createAccountHolder() {
+		Collection<Transaction> txnSet = new HashSet<>();
+		txnSet.add(new Transaction("id1", BigDecimal.valueOf(10.10), "ACC1", "USD"));
+		txnSet.add(new Transaction("id1", BigDecimal.valueOf(99.10), "ACC1", "USD"));
+		txnSet.add(new Transaction("id2", BigDecimal.valueOf(20.12), "ACC2", "USD"));
+		txnSet.add(new Transaction("id3", BigDecimal.valueOf(30.13), "ACC2", "EUR"));
+		txnSet.add(new Transaction("id4", BigDecimal.valueOf(40.14), "ACC3", "EUR"));
+		
+		AccountHolder ah = new AccountHolder();
+		txnSet.forEach(ah::process);
+		ah.accounts.entrySet().forEach(e-> {System.out.println(e.getKey() + ":" + e.getValue());});
+		
+	}
+	
+	@Test
+	public void transformCollectionToMapIgnoreDuplicates() throws ParseException {
+
+		final String id1 = "id1";
+		final Transaction id1LastVersion = new Transaction(id1, BigDecimal.valueOf(99.10), "ACC1.Last", "GBP", getDate("10.09.2016"));
+
+		Set<Transaction> txnSet = new HashSet<>();
+		txnSet.add(new Transaction(id1, BigDecimal.valueOf(10.10), "ACC1", "GBP", getDate("01.09.2016")));
+		txnSet.add(new Transaction("id2", BigDecimal.valueOf(20.12), "ACC2", "USD", getDate("01.09.2016")));
+		txnSet.add(new Transaction("id3", BigDecimal.valueOf(30.13), "ACC2", "EUR", getDate("01.09.2016")));
+		txnSet.add(new Transaction("id4", BigDecimal.valueOf(40.14), "ACC3", "EUR", getDate("01.09.2016")));
+		txnSet.add(id1LastVersion);
+		
+		
+		@SuppressWarnings("unused")
+		BinaryOperator<Transaction> mergeFunction = new BinaryOperator<Transaction>() {
+			@Override
+			public Transaction apply(Transaction oldValue, Transaction newValue) {
+				return oldValue.getLastUpdateDate().compareTo(newValue.getLastUpdateDate()) <=0 ? newValue : oldValue; 
+			}
+		};
+		// mergeFunction и mergeFunction1 делают одно и тоже, просто разная форма записи
+		BinaryOperator<Transaction> mergeFunction1 = (oldValue, newValue) ->{ return oldValue.getLastUpdateDate().compareTo(newValue.getLastUpdateDate()) <=0 ? newValue : oldValue; };
+		
+		// Если не указать mergeFunction, то данный запрос будет "падать", так как коллекция содержит повторяющиеся элементы
+		final Map<String, Transaction> collect = txnSet.stream()
+				.collect(Collectors.toMap(Transaction::getId, Function.identity(), mergeFunction1));
+
+		collect.values().stream().forEach(System.out::println);
+		assertEquals(4, collect.size());
+		
+		assertEquals(id1LastVersion, collect.get(id1));
+	}	
+
+	
+	@Test
+	public void transformCollectionToIdList() {
+		List<Transaction> txns = Arrays.asList(
+				new Transaction("id1", BigDecimal.valueOf(10.10), "ACC1", "USD"),
+				new Transaction("id2", BigDecimal.valueOf(20.12), "ACC2", "USD"),
+				new Transaction("id3", BigDecimal.valueOf(30.13), "ACC2", "EUR"),
+				new Transaction("id4", BigDecimal.valueOf(40.14), "ACC3", "EUR")
+				);
+		
+		List<String> array = txns.stream()
+							.filter(t-> t.getCurrency().equalsIgnoreCase("USD"))
+							.map(t-> t.getId())
+							.collect(Collectors.toList());
+		
+		assertEquals(2, array.size());
+	}
+
+	@Test
+	public void groupByAccountCurrencies() {
+		List<Transaction> txns = Arrays.asList(
+				new Transaction("id1", BigDecimal.valueOf(10.10), "ACC1", "USD"),
+				new Transaction("id2", BigDecimal.valueOf(20.12), "ACC2", "USD"),
+				new Transaction("id3", BigDecimal.valueOf(30.13), "ACC2", "EUR"),
+				new Transaction("id4", BigDecimal.valueOf(20.00), "ACC2", "EUR"),
+				new Transaction("id5", BigDecimal.valueOf(40.14), "ACC3", "EUR")
+				);
+	
+		// Счет, список валют
+		Map<String, List<String>> collect = 
+				txns.stream()
+			           .collect(Collectors.groupingBy(
+			        		   Transaction::getAccount,
+			               Collectors.mapping(Transaction::getCurrency, Collectors.toList()))
+			           );
+		
+		for (Entry<String, List<String>> e : collect.entrySet()) {
+			System.out.println(e.getKey() + ":" + e.getValue());
+		}
+		
+		assertEquals(3, collect.size());
+	}
+	
+	@Test
+	public void groupByTotalAmountByCurrency() {
+		List<Transaction> txns = Arrays.asList(
+				new Transaction("id1", BigDecimal.valueOf(10.10), "ACC1", "USD"),
+				new Transaction("id2", BigDecimal.valueOf(20.12), "ACC2", "USD"),
+				new Transaction("id3", BigDecimal.valueOf(30.13), "ACC2", "EUR"),
+				new Transaction("id4", BigDecimal.valueOf(20.00), "ACC2", "EUR"),
+				new Transaction("id5", BigDecimal.valueOf(40.14), "ACC3", "EUR")
+				);
+	
+		Map<String, BigDecimal> c = txns.stream()
+				.collect(
+				Collectors.groupingBy(Transaction::getCurrency,
+				Collectors.reducing(BigDecimal.ZERO, Transaction::getAmount, BigDecimal::add))
+				);
+		
+		for (Entry<String, BigDecimal> e : c.entrySet()) {
+			System.out.println(e.getKey() + ":" + e.getValue());
+		}
+		
+		assertEquals(2, c.size());
+	}
+	
+	@Test
+	public void groupByAccountAmountList() {
+		List<Transaction> txns = Arrays.asList(
+				new Transaction("id1", BigDecimal.valueOf(10.10), "ACC1", "USD"),
+				new Transaction("id2", BigDecimal.valueOf(20.12), "ACC2", "USD"),
+				new Transaction("id3", BigDecimal.valueOf(30.13), "ACC2", "EUR"),
+				new Transaction("id4", BigDecimal.valueOf(20.00), "ACC2", "EUR"),
+				new Transaction("id5", BigDecimal.valueOf(40.14), "ACC3", "EUR")
+				);
+	
+		Map<String, List<Amount>> collect = 
+				txns.stream()
+			           .collect(Collectors.groupingBy(Transaction::getAccount,
+			               Collectors.mapping(Amount::new, Collectors.toList())
+			               )
+			           );
+			
+		for (Entry<String, List<Amount>> e : collect.entrySet()) {
+			System.out.println(e.getKey() + ":" + e.getValue());
+		}
+
+		assertEquals(3, collect.size());
+	}
+	
+	@Test
+	public void totalAmountByCurrency() {
+		Set<Amount> set = new HashSet<>();
+		set.add(new Amount("EUR", BigDecimal.valueOf(30.13)));
+		set.add(new Amount("EUR", BigDecimal.valueOf(20.0)));
+		set.add(new Amount("USD", BigDecimal.valueOf(20.12)));
+		set.add(new Amount("USD", BigDecimal.valueOf(50)));
+		set.add(new Amount("GBP", BigDecimal.valueOf(150)));
+		
+		Map<String, Optional<Amount>> r = set.stream()
+				.collect(
+						Collectors.groupingBy(Amount::getCurrency,
+						Collectors.reducing(Amount::increase))
+						);
+						
+		for (Entry<String, Optional<Amount>> e : r.entrySet()) {
+			System.out.println(e.getKey() + ":" + e.getValue());
+		}
+		assertEquals(3,r.size());
+	}
+
+
+	class AccountHolder {
+
+		Map<String, Account> accounts = new HashMap<>();
+
+		public void process(Transaction txn) {
+			String accId = txn.getAccount();
+			Account account = null;
+			if ( accounts.containsKey(accId)) {
+				account  = accounts.get(accId);
+			} else {
+				account  = new Account(accId);
+			}
+			account.increase(txn.getCurrency(), txn.getAmount());
+			accounts.put(accId, account);
+		}
+		
+	}
+	
+	class Account {
+		private String id;
+		Map<String, Amount> amounts = new HashMap<>();
+		
+		public Account(String accId) {
+			this.id = accId;
+		}
+
+		public void increase(String currency, BigDecimal amount) {
+			if (amounts.containsKey(currency)) {
+				amounts.get(currency).increase(amount);
+			} else {
+				amounts.put(currency, new Amount(currency, amount));
+			}
+		}
+
+		@Override
+		public String toString() {
+			return "Account [id=" + id + ", amounts=" + amounts + "]";
+		}
+	}
+	
+	class Amount {
+		private String currency;
+		private BigDecimal totalAmount = BigDecimal.ZERO;
+		
+		Amount(Transaction txn) {
+			this.currency = txn.getCurrency();
+			this.totalAmount = txn.getAmount();
+		}
+		
+		Amount(String currency, BigDecimal amount) {
+			this.currency = currency;
+			this.totalAmount = amount;
+		}
+		
+		public void increase(BigDecimal amount) {
+			this.totalAmount = totalAmount.add(amount);
+		}
+		
+		public Amount increase(Amount totalAmount) {
+			increase(totalAmount.getTotalAmount());
+			return this;
+		}
+		
+		public String getCurrency() {
+			return currency;
+		}
+
+		public BigDecimal getTotalAmount() {
+			return totalAmount;
+		}
+
+		@Override
+		public String toString() {
+			return "TotalAmount [currency=" + currency + ", totalAmount=" + totalAmount + "]";
+		}
+	}
+	
+	static class TxnProcessor {
+		public void simpleMethod(Transaction t) {
+			System.out.println(t);
+		}
+		public static void staticMethod(BigDecimal amount, String currency) {
+			System.out.println(amount + ":" + currency);
+		}
+	}
+	
+	static class MyProcessor implements Consumer<Transaction> {
+
+		@Override
+		public void accept(Transaction t) {
+			System.out.println("#" + t);
+		}
+
+	}
+	
+	private Date getDate(String date) throws ParseException {
+		return new SimpleDateFormat("dd.MM.yyyy").parse(date);
+	}
+
+	
+}
