dc86e0d0cf8b9708a735b4140da0e23e86096d22
nixx78 <nixx78@gmail.com>
2016-10-24 11:15:09 +0300

    Bitmask usage sample added

---
+++ java8-poc/src/test/java/lv/nixx/poc/java8/collection/BitmaskUsageSample.java
@@ -0,0 +1,61 @@
+package lv.nixx.poc.java8.collection;
+
+import static org.junit.Assert.*;
+
+import static lv.nixx.poc.java8.collection.BitmaskUsageSample.Permissions.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+public class BitmaskUsageSample {
+
+	@Test
+	public void parseValueIntoPermissions() {
+		Collection<Permissions> p = getPermissions(1+ 8);
+		assertNotNull(p);
+		assertEquals(new LinkedHashSet<>(Arrays.asList(WRITE, SEARCH)), p);
+	}
+	
+	@Test
+	public void parseAllPossibleValueIntoPermissions() {
+		Collection<Permissions> p = getPermissions(1 + 2 + 4 + 8);
+		assertNotNull(p);
+		assertEquals(new LinkedHashSet<>(Arrays.asList(WRITE, READ, SEARCH, DELETE)), p);
+	}
+	
+	@Test 
+	public void parseNullValue() {
+		final Collection<Permissions> p = getPermissions(0);
+		assertNotNull(p);
+		assertTrue(p.isEmpty());
+	}
+	
+	private Collection<Permissions> getPermissions(int value) {
+		final Set<Permissions> permissions = Arrays.stream(Permissions.values())
+				.filter(p -> (p.bit & value) > 0)
+				.collect(Collectors.toSet());
+		
+		return permissions;
+	}
+
+	enum Permissions {
+		// Values also can be set as decimal values
+		WRITE (0b00001), // 1 
+		READ  (0b00010), // 2
+		DELETE(0b00100), // 4
+		SEARCH(0b01000); // 8
+
+		int bit;
+
+		Permissions(int bit) {
+			this.bit = bit;
+		}
+
+	}
+
+}
