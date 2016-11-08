1faa1e2c0afd19282410ac04fca761e950285525
nixx78 <nixx78@gmail.com>
2016-11-08 09:56:41 +0200

    More lambda added to sample

--- java8-poc/src/main/java/lv/nixx/poc/java8/collection/txn/ThreadLocalLambda.java
+++ java8-poc/src/main/java/lv/nixx/poc/java8/collection/txn/ThreadLocalLambda.java
@@ -2,7 +2,9 @@ package lv.nixx.poc.java8.collection.txn;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -11,30 +13,35 @@ public class ThreadLocalLambda {
 	ThreadLocal<DateFormat> df = ThreadLocal.withInitial(() -> new SimpleDateFormat("ddMMyyyy"));
 	
 	@Test
-	public void test() throws Exception {
+	public void test() {
 		
-		Runnable r1 = () -> {
-			try {
-				System.out.println(Thread.currentThread().getName() + ":" + df.get().parse("01122016"));
-				synchronized (this) {
-					wait(200);
-				}
-				System.out.println("R1 End");
-			} catch (Exception e) {
-				e.printStackTrace();
-		}};
+		Runnable[] ra = { 
+				() -> {
+					try {
+						System.out.println(Thread.currentThread().getName() + ":" + df.get().parse("01122016"));
+						synchronized (this) {
+							wait(200);
+						}
+						System.out.println("R1 End");
+					} catch (Exception e) {
+						e.printStackTrace();
+				}},
+				() -> {
+					try {
+						System.out.println(Thread.currentThread().getName() + ":" + df.get().parse("02122016"));
+					} catch (Exception e) {
+						e.printStackTrace();
+				}}
+		};		
+		
+		List<Thread> threads = new ArrayList<>();
+		
+		Arrays.stream(ra).map(Thread::new).forEach(t -> {
+			threads.add(t);
+			t.start();
+		});
 
-		Runnable r2 = () -> {
-			try {
-				System.out.println(Thread.currentThread().getName() + ":" + df.get().parse("02122016"));
-			} catch (Exception e) {
-				e.printStackTrace();
-		}};
-
-		Thread[] ta = {new Thread(r1), new Thread(r2)};
-		Arrays.stream(ta).forEach(t -> t.start());
-
-		Arrays.stream(ta).forEach(t -> {
+		threads.forEach(t -> {
 			try {
 				t.join();
 			} catch (Exception e) {
