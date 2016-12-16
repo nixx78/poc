04eca5370829706aa686dec90b04d26128b19a89
nixx78 <nixx78@gmail.com>
2016-12-16 11:45:29 +0200

    Trivial samples added, just for brain training :)

---
+++ sandbox/src/main/java/lv/nixx/poc/sandbox/binarysearch/BinarySearcher.java
@@ -0,0 +1,27 @@
+package lv.nixx.poc.sandbox.binarysearch;
+
+import java.util.*;
+
+public class BinarySearcher {
+	
+	public static Person searchById(List<Person> c, Integer searchId) {
+
+		Collections.sort(c, (p1, p2) -> Integer.compare(p1.id, p2.id));
+		boolean doIteration = true;
+		
+		while(doIteration) {
+			int midle = c.size() / 2;
+			final Person cp = c.get(midle);
+			
+			if (cp.id == searchId) {
+				return cp;
+			} else if ( midle == 0) {
+				return null;
+			}
+			c = searchId < cp.id  ? c.subList(0, midle) : c.subList(midle, c.size());
+		}
+		return null;
+	}
+	
+
+}
---
+++ sandbox/src/main/java/lv/nixx/poc/sandbox/binarysearch/Person.java
@@ -0,0 +1,18 @@
+package lv.nixx.poc.sandbox.binarysearch;
+
+public class Person {
+	
+	public int id;
+	public String name;
+	
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
+	
+}
