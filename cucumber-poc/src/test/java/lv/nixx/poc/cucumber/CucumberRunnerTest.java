2038a620e622324ae97af1a7b66f0b8730b3edf4
nixx78 <nixx78@gmail.com>
2017-06-16 08:45:40 +0300

    minor formating

--- cucumber-poc/src/test/java/lv/nixx/poc/cucumber/CucumberRunnerTest.java
+++ cucumber-poc/src/test/java/lv/nixx/poc/cucumber/CucumberRunnerTest.java
@@ -6,7 +6,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", plugin = { "pretty",
-		"html:target/cucumber" }, glue = "lv.nixx.poc.cucumber.stepdef")
+@CucumberOptions(
+		features = "src/test/resources/features", 
+		plugin = { "pretty", "html:target/cucumber" }, 
+		glue = "lv.nixx.poc.cucumber.stepdef")
 public class CucumberRunnerTest {
 }
