ae4d0160ab89642cd9931505915dde1af18806c6
nixx78 <nixx78@gmail.com>
2017-10-16 20:39:54 +0300

    Minor changes in procedure name

--- java9-poc/src/test/java/lv/nixx/poc/java9/MyInterfaceTest.java
+++ java9-poc/src/test/java/lv/nixx/poc/java9/MyInterfaceTest.java
@@ -5,24 +5,23 @@ import org.junit.Test;
 public class MyInterfaceTest {
 
 	@Test
-	public void test() {
+	public void interfaceImpl() {
 		MyInterface c = new MyInterfaceImpl();
 		c.simpleMethod();
 		c.anotherMethodWithDefault();
 	}
 	
 	@Test
-	public void test1() {
+	public void overridenMethod() {
 		
 		MyInterface c = new MyInterfaceImpl( ) {
+			
 			public void methodWithDefault() {
 				System.out.println("methodWithDefault:another impl");
 			}
 		};
-		
 		c.simpleMethod();
 		c.methodWithDefault();
-		
 	}
 	
 }
