ebacdc6b28a3f900dca5058271f21d14f13e8fa3
nixx78 <nixx78@gmail.com>
2017-10-18 18:48:39 +0300

    New samples added

---
+++ java9-poc/src/test/java/lv/nixx/poc/java9/TryWithResources.java
@@ -0,0 +1,33 @@
+package lv.nixx.poc.java9;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class TryWithResources {
+
+	@Test
+	public void java17Sample() throws IOException {
+		try (Closeable c = new CloseableImpl()) {
+			System.out.println("Process");
+		}
+	}
+
+	@Test
+	public void java19Sample() throws IOException {
+		Closeable c = new CloseableImpl();
+		try (c) {
+			System.out.println("Process");
+		}
+	}
+
+	class CloseableImpl implements Closeable {
+
+		@Override
+		public void close() throws IOException {
+			System.out.println("Closeable called");
+		}
+		
+	}
+}
