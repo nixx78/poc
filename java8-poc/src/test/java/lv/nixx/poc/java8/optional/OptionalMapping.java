3f08f8f98e77df0cc482625f9bcf0862e4262834
nixx78 <nixx78@gmail.com>
2018-03-11 19:26:07 +0200

    Mapping sample for Optional added

--- java8-poc/src/test/java/lv/nixx/poc/java8/optional/OptionalMapping.java
+++ java8-poc/src/test/java/lv/nixx/poc/java8/optional/OptionalMapping.java
@@ -2,7 +2,11 @@ package lv.nixx.poc.java8.optional;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
@@ -51,11 +55,41 @@ public class OptionalMapping {
 
 	@Test
 	public void optionalFilter() {
-		
+
 		Optional<String> s = Optional.of("xyz");
-		final Optional<String> filterOpt = s.filter( t-> t.equalsIgnoreCase("xyz"));
-		
+		final Optional<String> filterOpt = s.filter(t -> t.equalsIgnoreCase("xyz"));
+
 		assertTrue(filterOpt.isPresent());
+	}
+
+	@Test
+	public void optionalMap() {
+
+		final Optional<Integer> optNotPresent = Optional.of(new Person("name.value", new Address("street.value", null)))
+				.map(Person::getAddress).map(Address::getHouseNum);
+
+		assertFalse(optNotPresent.isPresent());
+
+		final Optional<Integer> optPresent = Optional.of(new Person("name.value", new Address("street.value", 100)))
+				.map(Person::getAddress).map(Address::getHouseNum);
+
+		assertTrue(optPresent.isPresent());
+		assertEquals((int) 100, (int) optPresent.get());
+
+		Collection<Person> persons = Arrays.asList(new Person("name.value0", new Address("street.value0", 100)),
+				new Person("name.value1", new Address("street.value1", null)),
+				new Person("name.value2", new Address("street.value2", 200)),
+				new Person("name.value3", new Address("street.value3", 300)));
+
+		final List<Integer> collect = persons.stream()
+				.map(Person::getAddress)
+				.map(Address::getHouseNum)
+				.filter(t -> t != null)
+				.filter(t -> t > 200)
+				.collect(Collectors.toList());
+
+		collect.forEach(System.out::println);
+
 	}
 
 	private String getElseValue() {
@@ -73,6 +107,44 @@ public class OptionalMapping {
 		String getValue() {
 			return aVal + "returnB";
 		}
+	}
+
+	class Person {
+		String name;
+		Address address;
+
+		Person(String name, Address address) {
+			this.name = name;
+			this.address = address;
+		}
+
+		String getName() {
+			return name;
+		}
+
+		Address getAddress() {
+			return address;
+		}
+
+	}
+
+	class Address {
+		String street;
+		Integer houseNum;
+
+		Address(String street, Integer houseNum) {
+			this.street = street;
+			this.houseNum = houseNum;
+		}
+
+		String getStreet() {
+			return street;
+		}
+
+		Integer getHouseNum() {
+			return houseNum;
+		}
+
 	}
 
 }
