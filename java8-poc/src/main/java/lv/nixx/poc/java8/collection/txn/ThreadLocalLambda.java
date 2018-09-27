0146e3dbfae72a0ad42997a584354e90304c8515
nixx78 <nixx78@gmail.com>
2018-09-27 17:08:32 +0300

    Sample with comparator added

--- java8-poc/src/main/java/lv/nixx/poc/java8/collection/txn/ThreadLocalLambda.java
+++ java8-poc/src/main/java/lv/nixx/poc/java8/collection/txn/ThreadLocalLambda.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+
 public class ThreadLocalLambda {
 	
 	ThreadLocal<DateFormat> df = ThreadLocal.withInitial(() -> new SimpleDateFormat("ddMMyyyy"));
