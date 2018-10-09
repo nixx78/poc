f5157f28038ca6e6e9beb9c1262761ce050f64ab
nixx78 <nixx78@gmail.com>
2018-10-09 10:37:21 +0300

    Sample project with SL4J & Log4j2 added

---
+++ sl4j-log4j-poc/src/main/java/lv/nixx/poc/Application.java
@@ -0,0 +1,18 @@
+package lv.nixx.poc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+	
+	
+	private static final Logger log = LoggerFactory.getLogger("AppLogger");
+	
+	public static void main(String[] args) {
+		log.debug("Simple message DEBUG");
+		log.debug("Simple message SYSTEM_X DEBUG");
+		log.info("INFO message");
+		log.error("ERROR message");
+	}
+
+}
