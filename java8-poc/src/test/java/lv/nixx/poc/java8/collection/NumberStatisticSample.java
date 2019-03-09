40abd35f5c6dd3abce8de3c6f3931b3c60324a50
nixx78 <nixx78@gmail.com>
2019-03-09 15:37:43 +0200

    Benchmark test added for number statistic

--- java8-poc/src/test/java/lv/nixx/poc/java8/collection/NumberStatisticSample.java
+++ java8-poc/src/test/java/lv/nixx/poc/java8/collection/NumberStatisticSample.java
@@ -4,55 +4,99 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.After;
 import org.junit.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 
+@State(value = Scope.Benchmark)
 public class NumberStatisticSample {
-	
-	private final Collection<String> input = Arrays.asList(
-			"11", "aa1", "2", "2x", "123", "3333", "3x", "x3x", "z3z2"
-			);
 
+	
+	private final Collection<String> input = Arrays.asList("", "11", "aa1", "2", "2x", "123", "3333", "3x", "x3x", "z3z2", "55678", "XXX");
+	
 	@After
 	public void after() {
 		System.out.println("===============");
 	}
-	
-	//TODO Add performance monitoring there
+
+	@Test
+	public void launchBenchmark() throws Exception {
+
+		Options opt = new OptionsBuilder()
+				// Specify which benchmarks to run.
+				// You can be more specific if you'd like to run only one benchmark per test.
+				.include(this.getClass().getName() + ".*")
+				// Set the following options as needed
+				.mode(Mode.AverageTime)
+				.timeUnit(TimeUnit.MICROSECONDS)
+				.warmupTime(TimeValue.seconds(1))
+				.warmupIterations(2)
+				.measurementTime(TimeValue.seconds(1))
+				.measurementIterations(2).threads(1).forks(1)
+				.shouldFailOnError(true)
+				.shouldDoGC(true)
+				// .jvmArgs("-XX:+UnlockDiagnosticVMOptions", "-XX:+PrintInlining")
+				// .addProfiler(WinPerfAsmProfiler.class)
+				.build();
+
+		new Runner(opt).run();
+	}
+
+
 	// Посчитать количество элементов с вхождением различных символов,
 	// например, 1an, 222as, 213 = "1" -> 2, "2" -> 2, "3" -> 1
 
-	
 	@Test
+	@Benchmark
 	public void firstApproach() {
 
-		final Collection<String> checkedChars = Arrays.asList(
-				"1", "2", "3"
-				);
+		final Collection<String> checkedChars = Arrays.asList("1", "2", "3");
 
 		final Map<String, Integer> statisic = new HashMap<>();
-		
+
 		for (String t : input) {
-			checkedChars.forEach( chr -> {
+			checkedChars.forEach(chr -> {
 				if (t.contains(chr)) {
-					statisic.merge(chr, 1, (t1, t2) -> t1 + t2); 
+					statisic.merge(chr, 1, (t1, t2) -> t1 + t2);
 				}
 			});
 		}
-		statisic.entrySet().forEach(System.out::println);
+		// statisic.entrySet().forEach(System.out::println);
 	}
-	
 
 	@Test
+	@Benchmark
 	public void secondApproach() {
-		final Map<String, Long> statisic = Stream.of("1", "2", "3")
-				.collect(Collectors.toMap(c-> c, c-> input.stream().filter(t -> t.contains(c)).count()));
+		final Map<String, Long> statisic = Stream.of("1", "2", "3").collect(Collectors.toMap(c -> c, c -> input.stream().filter(t -> t.contains(c)).count()));
+		//statisic.entrySet().forEach(System.out::println);
+	}
+	
+	@Test
+	@Benchmark
+	public void thirdApproach() {
+		
+		final Map<String, Integer> statisic = new HashMap<>();
 
-		statisic.entrySet().forEach(System.out::println);
+		Stream.of("1", "2", "3").forEach( t -> {
+			for(String s: input) {
+				if (s.contains(t)) {
+					statisic.merge(t, 1, (t1, t2) -> t1 + t2);
+				}
+				
+			}
+		});
+		//statisic.entrySet().forEach(System.out::println);
 	}
 
 }
-
