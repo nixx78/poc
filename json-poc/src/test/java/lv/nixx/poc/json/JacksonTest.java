7e683f509f22d361a99ec93091ffbfec51d6b47c
nixx78 <nixx78@gmail.com>
2019-09-18 11:15:17 +0300

    JsonNode compare sample added

--- json-poc/src/test/java/lv/nixx/poc/json/JacksonTest.java
+++ json-poc/src/test/java/lv/nixx/poc/json/JacksonTest.java
@@ -1,9 +1,11 @@
 package lv.nixx.poc.json;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
 
 import lv.nixx.poc.domain.*;
@@ -39,13 +41,36 @@ public class JacksonTest {
 	public void requiredFieldTest() throws Exception {
 		String json = "{\r\n" + 
 				"  \"id\" : 10,\r\n" + 
-				"  \"amount\" : 10.0\r\n" + 
+				"  \"amount\" : 10.0,\r\n" +
+				"  \"currency\": null" +
 				"}";
 		
 		Transaction txn = objectMapper.readValue(json, Transaction.class);
 		assertNotNull(txn);
 		
 		System.out.println(txn);
+	}
+
+	@Test
+	public void jsonNodeCompareSuccessTest() throws IOException {
+		String expectedJson = "{\"id\":10,\"amount\":10.0}";
+		String   actualJson = "{\"amount\":10.0,\"id\":10}";
+
+		JsonNode expectedNode = objectMapper.readTree(expectedJson);
+		JsonNode actualNode = objectMapper.readTree(actualJson);
+
+		assertEquals(expectedNode, actualNode);
+	}
+
+	@Test
+	public void jsonNodeCompareFailTest() throws IOException {
+		String expectedJson = "{\"id\":10,\"amount\": 10.0}";
+		String actualJson   = "{\"id\":10,\"amount\": 10.1}";
+
+		JsonNode expectedNode = objectMapper.readTree(expectedJson);
+		JsonNode actualNode = objectMapper.readTree(actualJson);
+
+		assertNotEquals(expectedNode, actualNode);
 	}
 
 }
