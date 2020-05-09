48a115afde615d85347e020382d4ce06fc8bd3ad
nixx78 <nixx78@gmail.com>
2020-05-09 10:57:47 +0300

    Resilience POC project initial commit

---
+++ resilience4j-poc/src/main/java/lv/nixx/poc/resilience/RetrySandbox.java
@@ -0,0 +1,55 @@
+package lv.nixx.poc.resilience;
+
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+public class RetrySandbox {
+
+    @Test
+    public void sandbox() {
+
+        RetryConfig c = RetryConfig.custom()
+                .maxAttempts(10)
+                .waitDuration(Duration.ofSeconds(5L))
+                .retryOnException(e -> e instanceof IllegalStateException)
+                .build();
+
+        final Retry retry = Retry.of("retry1", c);
+
+        final Retry.EventPublisher ep = retry.getEventPublisher();
+
+        ep.onRetry(e -> {
+            System.out.println("Retry event, type:" + e.getEventType() + ", try:" + e.getNumberOfRetryAttempts() + " error: " + e.getLastThrowable());
+        });
+
+        ep.onSuccess(e -> {
+            System.out.println("Retry event, type:" + e.getEventType() + ", try:" + e.getNumberOfRetryAttempts() + " error: " + e.getLastThrowable());
+        });
+
+        Service service = new Service();
+
+        final String result = retry.executeSupplier(() -> service.method("Param1"));
+        System.out.println("Result:" + result);
+    }
+
+    class Service {
+        int i = 0;
+
+        String method(String param) {
+            System.out.println("Method call, param:" + param);
+            try {
+                if (i < 3) {
+                    throw new IllegalStateException("Exception in service");
+                }
+                return "Success:" + param;
+            } finally {
+                i++;
+            }
+        }
+    }
+
+
+}
