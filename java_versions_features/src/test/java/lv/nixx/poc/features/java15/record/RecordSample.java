84fb01006bc58d85a3e40fe0b36efeddcd9c24c3
nixx78 <nixx78@gmail.com>
2021-02-21 20:01:42 +0200

    More record samples added

--- java_versions_features/src/test/java/lv/nixx/poc/features/java15/record/RecordSample.java
+++ java_versions_features/src/test/java/lv/nixx/poc/features/java15/record/RecordSample.java
@@ -3,8 +3,10 @@ package lv.nixx.poc.features.java15.record;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static java.math.BigDecimal.ZERO;
+import static org.junit.Assert.assertTrue;
 
 public class RecordSample {
 
@@ -24,16 +26,53 @@ public class RecordSample {
         new Transaction(1, ZERO);
     }
 
+    @Test
+    public void staticFactoryMethodSample() {
+        Transaction t = Transaction.of(BigDecimal.valueOf(100));
+        System.out.println(t);
+    }
+
+    @Test
+    public void oneParameterConstructorSample() {
+        Transaction t = new Transaction(BigDecimal.valueOf(-777));
+        System.out.println(t);
+    }
+
+    @Test
+    public void recordsReflect() {
+        assertTrue(Transaction.class.isRecord());
+        Stream.of(Transaction.class.getRecordComponents()).forEach(System.out::println);
+    }
+
+
 }
 
 record Transaction(int id, BigDecimal amount) {
 
+    // Простое поле добавить не возможно
+    // private String vl;
+
+    // Можно добавить статическое поле
+    private static String s;
+
+    // Альтернативный конструктор
+    public Transaction(BigDecimal amount) {
+        this(-1, amount);
+    }
+
     Transaction {
+        // в конструкторе, мы можем произвести проверку параметров
         if (amount.compareTo(ZERO) == 0) {
             throw new IllegalArgumentException("Amount can't be zero");
         }
     }
 
+    // Можно создавать статические методы для инициации
+    static Transaction of(BigDecimal amount) {
+        return new Transaction(-1, amount);
+    }
+
+    // Можно создавать методы
     String getDescription() {
         return "Transaction id:" + id + "amount: " + amount;
     }
