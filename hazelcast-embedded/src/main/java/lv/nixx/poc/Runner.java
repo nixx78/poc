2f7c94c1a1d2dcf2793ac4a97dfe9bb5d387539c
nixx78 <nixx78@gmail.com>
2021-03-10 19:49:08 +0200

    New project for Embedded Hazelcast playground added

---
+++ hazelcast-embedded/src/main/java/lv/nixx/poc/Runner.java
@@ -0,0 +1,15 @@
+package lv.nixx.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//TODO https://docs.hazelcast.com/imdg/latest/management/rest-endpoint-groups.html
+
+@SpringBootApplication
+public class Runner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Runner.class, args);
+	}
+
+}
