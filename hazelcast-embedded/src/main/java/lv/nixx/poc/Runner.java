3fde25a51851f6961edef51eacfd8fe9ac048d2b
nixx78 <nixx78@gmail.com>
2021-03-21 09:26:56 +0200

    Hazelcast Jet sandbox added

--- hazelcast-embedded/src/main/java/lv/nixx/poc/Runner.java
+++ hazelcast-embedded/src/main/java/lv/nixx/poc/Runner.java
@@ -1,11 +1,14 @@
 package lv.nixx.poc;
 
+import com.hazelcast.jet.JetInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //TODO https://docs.hazelcast.com/imdg/latest/management/rest-endpoint-groups.html
 // https://docs.hazelcast.com/management-center/4.2021.03/clustered-rest.html
 // https://docs.hazelcast.com/imdg/latest/clients/rest.html
+// https://www.baeldung.com/hazelcast-jet
 
 @SpringBootApplication
 public class Runner {
---
+++ hazelcast-embedded/src/test/java/lv/nixx/poc/PipelineSandbox.java
@@ -0,0 +1,57 @@
+package lv.nixx.poc;
+
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.Sinks;
+import com.hazelcast.jet.pipeline.Sources;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.hazelcast.function.Functions.*;
+import static com.hazelcast.jet.Traversers.traverseArray;
+import static com.hazelcast.jet.aggregate.AggregateOperations.counting;
+import static org.junit.Assert.assertEquals;
+
+public class PipelineSandbox {
+
+
+    private final String LIST_NAME = "wordList";
+    private final String MAP_NAME = "wordMap";
+
+    private Pipeline createPipeLine() {
+        Pipeline p = Pipeline.create();
+        p.readFrom(Sources.<String>list(LIST_NAME))
+                .flatMap(word -> traverseArray(word.toLowerCase().split("\\W+")))
+                .filter(word -> !word.isEmpty())
+                .groupingKey(wholeItem())
+                .aggregate(counting())
+                .writeTo(Sinks.map(MAP_NAME));
+        return p;
+    }
+
+    public Long countWord(List<String> sentences, String word) {
+        long count = 0;
+        JetInstance jet = Jet.newJetInstance();
+        try {
+            List<String> textList = jet.getList(LIST_NAME);
+            textList.addAll(sentences);
+            Pipeline p = createPipeLine();
+            jet.newJob(p).join();
+            Map<String, Long> counts = jet.getMap(MAP_NAME);
+            count = counts.get(word);
+        } finally {
+            Jet.shutdownAll();
+        }
+        return count;
+    }
+
+    @Test
+    public void test() {
+        long c = countWord(List.of("A", "A", "B", "C", "D", "A"), "a");
+        assertEquals(3L, c);
+    }
+
+}
