2a6e2db5a3a24de7fa4b65e54b156e74be5223d7
nixx78 <nixx78@gmail.com>
2021-07-02 11:28:36 +0300

    Group & Reduce sample added

---
+++ sandbox/src/test/java/lv/nixx/poc/sandbox/collection/GroupSample.java
@@ -0,0 +1,65 @@
+package lv.nixx.poc.sandbox.collection;
+
+import lombok.ToString;
+import lv.nixx.poc.domain.User;
+import org.junit.Test;
+
+import java.util.*;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.reducing;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+
+public class GroupSample {
+
+    @Test
+    public void groupSample() {
+
+        Collection<User> users = List.of(
+                new User(10, "name", "user1"),
+                new User(10, "group", "group1"),
+                new User(10, "schema", "schema1"),
+                new User(20, "name", "user2"),
+                new User(20, "group", "group2"),
+                new User(20, "schema", "schema2")
+        );
+
+        Map<Integer, UserDTO> userDTO = users.stream()
+                .collect(
+                        groupingBy(User::getId,
+                                reducing(new UserDTO(), UserDTO::new, (dto1, dto2) -> dto1.putAll(dto2.values))
+                        )
+                );
+
+        System.out.println(userDTO.values());
+        assertEquals(2, userDTO.size());
+        assertThat(userDTO.get(10).values.entrySet(), is(Map.of(
+                "name", "user2",
+                "group", "group2",
+                "schema", "schema2"
+        ).entrySet()));
+    }
+
+    @ToString
+    static class UserDTO {
+        int id;
+        Map<String, String> values = new HashMap<>();
+
+        public UserDTO() {
+        }
+
+        public UserDTO(User user) {
+            this.id = user.getId();
+            values.put(user.getAttribute(), user.getValue());
+        }
+
+        private UserDTO putAll(Map<String, String> v) {
+            values.putAll(v);
+            return this;
+        }
+    }
+
+
+}
