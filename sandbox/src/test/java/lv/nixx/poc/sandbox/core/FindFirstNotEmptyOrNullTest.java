d386e262972a63114c51cfbb02b9e9c2f61d8aa4
nixx78 <nixx78@gmail.com>
2021-10-22 14:32:26 +0300

    Simple test for find first added

---
+++ sandbox/src/test/java/lv/nixx/poc/sandbox/core/FindFirstNotEmptyOrNullTest.java
@@ -0,0 +1,38 @@
+package lv.nixx.poc.sandbox.core;
+
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class FindFirstNotEmptyOrNullTest {
+
+    @Test
+    public void test() {
+        assertEquals("F", new Parameters("", null, "F", "X").getFirstNotEmpty());
+        assertNull(new Parameters("", null, "", null).getFirstNotEmpty());
+        assertEquals("A", new Parameters("A", "B", "F", "X").getFirstNotEmpty());
+        assertNull(new Parameters(null, null, null, null).getFirstNotEmpty());
+    }
+
+    @AllArgsConstructor
+    static class Parameters {
+        String p1;
+        String p2;
+        String p3;
+        String p4;
+
+        String getFirstNotEmpty() {
+            return Stream.of(new String[]{p1, p2, p3, p4})
+                    .filter(Objects::nonNull)
+                    .filter(t -> !t.isEmpty())
+                    .findFirst().orElseGet(() -> null);
+        }
+    }
+
+
+}
