c82e9364a4be6c34ed827641faf43e5f30306ac4
nixx78 <nixx78@gmail.com>
2023-03-17 13:41:45 +0200

    Test commit 1

---
+++ sandbox/src/test/java/lv/nixx/poc/sandbox/collection/MapWithCollectionTest.java
@@ -0,0 +1,23 @@
+package lv.nixx.poc.sandbox.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+class MapWithCollectionTest {
+
+    @Test
+    void operations() {
+        Map<String, Collection<String>> map = new HashMap<>();
+
+        map.computeIfAbsent("k1", k -> new ArrayList<>()).add("v1");
+        map.computeIfAbsent("k1", k -> new ArrayList<>()).add("v2");
+        map.computeIfAbsent("k2", k -> new ArrayList<>()).add("v2");
+
+        System.out.println(map);
+}
+
+}
