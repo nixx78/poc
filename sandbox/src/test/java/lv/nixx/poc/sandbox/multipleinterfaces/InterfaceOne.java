618936d9636a0c1bffcf6141f14080c235d75c71
nixx78 <nixx78@gmail.com>
2023-12-29 21:47:56 +0200

    Project refactoring. Moved classes deleted from project

--- sandbox/src/test/java/lv/nixx/poc/sandbox/multipleinterfaces/InterfaceOne.java
+++
@@ -1,7 +0,0 @@
-package lv.nixx.poc.sandbox.multipleinterfaces;
-
-public interface InterfaceOne {
-    void method1(String p);
-
-    void method2(String p);
-}
--- sandbox/src/test/java/lv/nixx/poc/sandbox/multipleinterfaces/InterfaceTwo.java
+++
@@ -1,9 +0,0 @@
-package lv.nixx.poc.sandbox.multipleinterfaces;
-
-public interface InterfaceTwo {
-    void method1(String p);
-
-    void method2(String p);
-
-    void method3(String p);
-}
