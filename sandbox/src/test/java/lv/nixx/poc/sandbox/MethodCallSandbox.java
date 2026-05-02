edc2aa8f38906411e46137929f1c11e75e89b3e2
nixx78 <nixx78@gmail.com>
2026-05-02 10:48:35 +0300

    Minor sample added

---
+++ sandbox/src/test/java/lv/nixx/poc/sandbox/MethodCallSandbox.java
@@ -0,0 +1,68 @@
+package lv.nixx.poc.sandbox;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MethodCallSandbox {
+
+    @Test
+    void methodCallUsingReflectionsTest() {
+        assertAll(
+                () -> assertThat(invokeHelper(new ClassWithBigDecimal())).isEqualByComparingTo(BigDecimal.valueOf(100.00)),
+                () -> assertThat(invokeHelper(new ClassWithString())).isEqualByComparingTo(BigDecimal.valueOf(777.00)),
+                () -> assertThrows(IllegalStateException.class, () -> invokeHelper(new ClassWithDouble()))
+        );
+    }
+
+    private static BigDecimal invokeHelper(Object target) {
+
+        Class<?> returnType;
+        try {
+            Method m = target.getClass().getMethod("getAmount");
+            returnType = m.getReturnType();
+
+            if (returnType.equals(BigDecimal.class)) {
+                return (BigDecimal) m.invoke(target);
+            }
+
+            if (returnType.equals(String.class)) {
+                return new BigDecimal((String) m.invoke(target));
+            }
+
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        throw new IllegalStateException("Return type %s not supported".formatted(returnType));
+    }
+
+}
+
+class ClassWithBigDecimal {
+    public BigDecimal getAmount() {
+        return BigDecimal.valueOf(100.00);
+    }
+}
+
+class ClassWithString {
+
+    public String getAmount() {
+        return "777.00";
+    }
+}
+
+class ClassWithDouble {
+
+    public Double getAmount() {
+        return 10.999;
+    }
+
+}
